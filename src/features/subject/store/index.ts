8fedb9b5923119cd9d60e715e2f38b007047ae99
waherof29502 <39645652+waherof29502@users.noreply.github.com>
2023-03-22 23:11:19 +0800

    build(redux): add redux tookit

---
+++ src/features/subject/store/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from '@reduxjs/toolkit';
+//import page reducer
+export const reducer = {
+  subject: combineReducers({
+    //page reducer
+  })
+};
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+//util state reducer
+
+const rootReducer = {
+  //util sate reducer
+  //child state reducer
+};
+
+export const store = configureStore({
+  reducer: rootReducer
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
